4db8c9a5a595e63f3b70d5590c1408e6aeabc923
PrathamSahu <80022922+PrathamSahu@users.noreply.github.com>
2021-09-02 19:22:02 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./pages/header.js";
+import Intro from "./pages/intro.js";
+import About from "./pages/aboutme.js";
+import Connect from "./pages/connect.js";
+import Exp from "./pages/exp.js";
+import Edu from "./pages/edu.js";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <About />
+      <Exp />
+      <Edu />
+      <Connect />
+
+    </>
+  );
+}
+
+export default App;
