21a33724c71f76f5f81fdc40d3211a20298c3735
PrathamSahu <80022922+PrathamSahu@users.noreply.github.com>
2021-09-02 19:46:15 +0530

    Update connect.js

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -12,25 +12,25 @@ function connect() {
         <h1 className="heading">CONNECT WITH ME :</h1>
         <div className="icons">
           <div className="icon-wrap">
-            <a href="https://instagram.com/im_prakhar02">
+            <a href="https://instagram.com/im_pratham02">
               <img src={INSTA} alt="" />
             </a>
           <h4>Instagram</h4>
           </div>
           <div className="icon-wrap github">
-            <a href="https://github.com/PrakharSahu02">
+            <a href="https://github.com/PrathamSahu">
               <img src={GITHUB} alt="" />
             </a>
             <h4>GitHub</h4>
           </div>
           <div className="icon-wrap">
-            <a href="https://twitter.com/PRAKHAR_2039">
+            <a href="https://twitter.com/PRATHAM_2731">
               <img src={TWITTER} alt="" />
             </a>
             <h4>Twitter</h4>
           </div>
           <div className="icon-wrap link">
-            <a href="https://www.linkedin.com/in/prakhar-sahu-4519b8206">
+            <a href="https://www.linkedin.com/in/pratham-sahu-513a00206">
               <img src={LINKEDIN} alt="" />
             </a>
             <h4>Linkedin</h4>
